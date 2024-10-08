32245e93fa821435cb63edbf7727c46c4a0f60d2
mreshboboyev <mreshboboyev@gmail.com>
2024-10-08 17:01:39 +0500

    Some style sin UI Register action completed;

--- OnlineLearningPlatform.Application/DTOs/UserProfileDTO.cs
+++ OnlineLearningPlatform.Application/DTOs/UserProfileDTO.cs
@@ -2,7 +2,7 @@
 {
     public class UserProfileDTO
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string UserId { get; set; }
         public string? Website { get; set; }
         public string? Bio { get; set; }
